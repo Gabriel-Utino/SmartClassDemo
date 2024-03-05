885ec0c18d2053c75afc8e08579f7f6dac467d60
Gabriel Utino <110830104+Gabriel-Utino@users.noreply.github.com>
2024-03-05 09:46:54 -0300

    shiken

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sidebarToggle = document.getElementById('sidebar-toggle');
     const sidebar = document.getElementById('sidebar');
+    const subMenus = document.getElementById('subMenus'); // 新しいサイドバー要素を追加
 
     sidebarToggle.addEventListener('click', function () {
-        if (sidebar.style.width === '250px') {
+        if (sidebar.style.width === '500px') {
             sidebar.style.width = '0';
+            subMenus.style.width = '0'; // サイドバー2も非表示に
         } else {
             sidebar.style.width = '250px';
+            subMenus.style.width = '250px'; // サイドバー2も表示
         }
     });
 });
+
